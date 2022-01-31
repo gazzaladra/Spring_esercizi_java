951eaa366f807fa8ec97fea81429d08304d3d8fa
Sc8Lorenz0 <98454597+Sc8Lorenz0@users.noreply.github.com>
2022-01-31 18:59:45 +0100

    Create Dado_S.java

---
+++ Dado_S.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+public class Dado
+{
+  public Dado(int a)
+  {
+    facce = a;
+    generatore = new Random();
+  }
+  public Dado(){
+    facce = 6;
+    generatore = new Random();
+  }
+  public int lancia()
+  {
+      return 1 + generatore.nextInt(facce);
+  }
+  private Random generatore;
+  private int facce;
+}
