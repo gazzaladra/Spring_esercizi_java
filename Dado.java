e29280beef593a0b85b276dd4eb40f4da449fe66
gazzaladra <97680385+gazzaladra@users.noreply.github.com>
2022-01-31 18:55:32 +0100

    Add files via upload
    Esercizio dado scritto in due classi, in cui si possono scegliere il numero delle facce e quanti lanci fare (main: DadoTesting), ("costruzione": dado)

--- Dado.java
+++ Dado.java
@@ -1,19 +1,48 @@
-import java.util.Random;
-public class Dado
-{
-  public Dado(int a)
-  {
-    facce = a;
-    generatore = new Random();
-  }
-  public Dado(){
-    facce = 6;
-    generatore = new Random();
-  }
-  public int lancia()
-  {
-      return 1 + generatore.nextInt(facce);
-  }
-  private Random generatore;
-  private int facce;
-}
+package dado;
+
+public class Dado {
+
+	private int sei;
+	private int ripeti;
+	
+	
+	public Dado(int sei, int ripeti) {
+		this.sei=sei;
+		this.ripeti=ripeti;
+	}
+	
+	public Dado() {
+		sei =0;
+		ripeti=0;
+	}
+	
+
+	public int getsei() {
+		return sei;
+	}
+	public void setsei(int sei) {
+		this.sei = sei;
+	}
+	
+	public int getripeti() {
+		return ripeti;
+	}
+	
+	public void setripeti (int ripeti) {
+		this.ripeti = ripeti;
+	}
+	
+	public void stampa () {
+		
+		int dadoG = (int) (Math.random()*sei)+1;
+		System.out.println ("Numero uscito dal dado: "+ dadoG);
+	}
+	
+	public void ripeti () {
+		
+		for (int i=1; i<ripeti; i++) {
+			int dadoG = (int) (Math.random()*sei)+1;
+			System.out.println ("Numero uscito dal dado: "+ dadoG);
+		}
+	}
+}	
